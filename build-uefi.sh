e1b9e324740f740351a51c5980459b994cf93fc6
Matt DeVillier <matt.devillier@gmail.com>
2014-07-13 23:08:42 -0500

    build: add configs, build scripts, cfbs files for supported boards
    currently supported boards:
    
    snb/ivb: butterly, lumpy, parrot, stout, stumpy
    hsw: falco, leon, mccloud, monroe, panther, peppy, tricky, wolf
    bdw: auron_paine, auron_yuna, gandof, guado, lulu, rikku, samus, tidus
    byt: banjo, candy, clapper, enguarde, glimmer, gnawty, heli, kip,
         ninja, orco, quawks, squawks, sumo, swanky, winky
    
    Change-Id: I674c097f1c292ceca3d4dd9260c39264cc50d797
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>

---
+++ build-uefi.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#
+
+set -e
+
+hsw=("falco" "leon" "mccloud" "monroe" "panther" "peppy" "tricky" "wolf" \
+	"zako");
+bdw=("auron_paine" "auron_yuna" "gandof" "guado" "lulu" "rikku" "samus" \
+	"tidus");
+byt=("banjo" "candy" "clapper" "enguarde" "glimmer" "gnawty" "heli" \
+	"kip" "ninja" "orco" "quawks" "squawks" "sumo" "swanky" "winky");
+snb_ivb=("link" "parrot_ivb" "parrot_snb" "stumpy")
+
+if [ -z "$1" ]; then
+	build_targets=($(printf "%s " "${hsw[@]}" "${bdw[@]}" "${byt[@]}" "${snb_ivb[@]}"));
+else
+	build_targets=($@)
+fi
+
+for device in ${build_targets[@]}
+do
+	filename="coreboot_tiano-${device}-mrchromebox_`date +"%Y%m%d"`.rom"
+	rm -f ~/firmware/${filename}*
+	rm -rf ./build
+	cp configs/.config.${device}.uefi .config
+	make
+	cp ./build/coreboot.rom ./${filename}
+	cbfstool ${filename} print
+	md5sum ${filename} > ${filename}.md5
+	mv ${filename}* ~/firmware/
+	if [ "${device}" == "peppy" ]; then
+		filename="coreboot_tiano-${device}_elan-mrchromebox_`date +"%Y%m%d"`.rom"
+		rm -rf ./build
+		sed -i 's/# CONFIG_ELAN_TRACKPAD_ACPI is not set/CONFIG_ELAN_TRACKPAD_ACPI=y/' .config
+		make
+		cp ./build/coreboot.rom ./${filename}
+		cbfstool ${filename} print
+		md5sum ${filename} > ${filename}.md5
+		mv ${filename}* ~/firmware/
+	fi
+done
