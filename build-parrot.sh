2dcb22a260e0146f456dd1cf6e923a98430bed11
Matt DeVillier <matt.devillier@gmail.com>
2014-07-13 23:08:42 -0500

    build: add configs, build scripts, cfbs files for supported boards
    currently supported boards:
    auron_paine
    auron_yuna
    falco
    gandof
    guado
    leon
    lulu
    mccloud
    monroe
    ninja
    panther
    parrot
    peppy
    rikku
    samus
    stumpy
    tidus
    tricky
    wolf
    zako
    
    Change-Id: I674c097f1c292ceca3d4dd9260c39264cc50d797
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>

---
+++ build-parrot.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+#
+set -e
+filename="coreboot_seabios-parrot_snb-mrchromebox_`date +"%Y%m%d"`.rom"
+filename2="coreboot_seabios_duet-parrot_snb-mrchromebox_`date +"%Y%m%d"`.rom"
+filename3="coreboot_seabios-parrot_ivb-mrchromebox_`date +"%Y%m%d"`.rom"
+filename4="coreboot_seabios_duet-parrot_ivb-mrchromebox_`date +"%Y%m%d"`.rom"
+rm -f ~/firmware/${filename}*
+rm -f ~/firmware/${filename2}*
+rm -f ~/firmware/${filename3}*
+rm -f ~/firmware/${filename4}*
+rm -rf ./build
+cp configs/.config-parrot .config
+make
+if [ "$1" != "uefi" ]; then
+	cp ./build/coreboot.rom ./${filename}
+	cbfstool ${filename} add -f ./cbfs/bootorder.ssd -n bootorder -t raw
+	cbfstool ${filename} add -f ./cbfs/links.sbib -n links -t raw
+	cbfstool ${filename} add-int -i 3000 -n etc/boot-menu-wait
+	cbfstool ${filename} print
+	md5sum ${filename} > ${filename}.md5
+	mv ${filename}* ~/firmware/
+	cp ./build/coreboot.rom ./${filename3}
+	cbfstool ${filename3} remove -n pci8086,0106.rom
+	cbfstool ${filename3} add -f ./3rdparty/blobs/mainboard/google/parrot/snm_2137_ivb_parrot.dat -n pci8086,0106.rom -t optionrom
+	cbfstool ${filename3} add -f ./cbfs/bootorder.ssd -n bootorder -t raw
+	cbfstool ${filename3} add -f ./cbfs/links.sbib -n links -t raw
+	cbfstool ${filename3} add-int -i 3000 -n etc/boot-menu-wait
+	cbfstool ${filename3} print
+	md5sum ${filename3} > ${filename3}.md5
+	mv ${filename3}* ~/firmware/
+fi
+if [ "$1" != "legacy" ]; then
+	cp ./build/coreboot.rom ./${filename2}
+	cbfstool ${filename2} remove -n fallback/payload
+	cbfstool ${filename2} remove -n pci8086,0106.rom
+	cbfstool ${filename2} add-payload -n fallback/payload -f ./seabios-hswbdw-duet.bin.elf -c lzma
+	cbfstool ${filename2} add -f ./cbfs/show-boot-menu-duet -n etc/show-boot-menu -t raw
+	cbfstool ${filename2} add -f ./cbfs/bootorder.floppy -n bootorder -t raw
+	cbfstool ${filename2} add -f ./duet-snb-book-ssd.img -n floppyimg/tianocore.img.lzma -t raw -c lzma
+	cbfstool ${filename2} print
+	md5sum ${filename2} > ${filename2}.md5
+	mv ${filename2}* ~/firmware/
+	cp ./build/coreboot.rom ./${filename4}
+	cbfstool ${filename4} remove -n fallback/payload
+	cbfstool ${filename4} remove -n pci8086,0106.rom
+	cbfstool ${filename4} add-payload -n fallback/payload -f ./seabios-hswbdw-duet.bin.elf -c lzma
+	cbfstool ${filename4} add -f ./cbfs/show-boot-menu-duet -n etc/show-boot-menu -t raw
+	cbfstool ${filename4} add -f ./cbfs/bootorder.floppy -n bootorder -t raw
+	cbfstool ${filename4} add -f ./duet-ivb-book-ssd.img -n floppyimg/tianocore.img.lzma -t raw -c lzma
+	cbfstool ${filename4} print
+	md5sum ${filename4} > ${filename4}.md5
+	mv ${filename4}* ~/firmware/
+fi
