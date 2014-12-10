948aceccd3e6f86608ca10ba2bd1876322d0f3a3
Matt DeVillier <matt.devillier@gmail.com>
2014-12-10 10:25:32 -0600

    add configs, build scripts for peppy/falco
    Change-Id: I51aa71ad202db1b64080a140a66aecdd0363aec7
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>

---
+++ build-peppy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+filename="coreboot-seabios-peppy-`date +"%Y%m%d"`-md.rom"
+rm -f ${filename}*
+rm -rf ./build
+cp .config-peppy .config
+make
+if [ $? -eq 0 ]; then
+dd if=./3rdparty/mainboard/google/peppy/descriptor.bin of=./build/coreboot.rom conv=notrunc
+cp ./build/coreboot.rom ./${filename}
+cbfstool ${filename} add -f ./cbfs/boot-menu-wait -n etc/boot-menu-wait -t raw
+cbfstool ${filename} add -f ./cbfs/boot-menu-key -n etc/boot-menu-key -t raw
+cbfstool ${filename} add -f ./cbfs/boot-menu-message -n etc/boot-menu-message -t raw
+cbfstool ${filename} add -f ./cbfs/links -n links -t raw
+cbfstool ${filename} print
+md5sum ${filename} > ${filename}.md5
+fi
+
