93c4c4917d30a9ce3b0199f54023f78ee0643048
Matt DeVillier <matt.devillier@gmail.com>
2016-08-05 02:20:15 -0500

    soc/intel/broadwell: add ACPI backlight
    Change-Id: If93b7690ef36b5d19ca43957e8a1bef91ec5821d

--- src/soc/intel/broadwell/lpc.c
+++ src/soc/intel/broadwell/lpc.c
@@ -599,7 +599,10 @@ static void southcluster_inject_dsdt(device_t device)
 	}
 
 	if (gnvs) {
+		const struct i915_gpu_controller_info *gfx = intel_gma_get_controller_info();
 		acpi_create_gnvs(gnvs);
+		gnvs->ndid = gfx->ndid;
+		memcpy(gnvs->did, gfx->did, sizeof(gnvs->did));
 		acpi_save_gnvs((unsigned long)gnvs);
 		/* And tell SMI about it */
 		smm_setup_structures(gnvs, NULL, NULL);
