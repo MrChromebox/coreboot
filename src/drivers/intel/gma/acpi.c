a35da7f0d5b0d39c87e3b741c20ac03b6ab55736
Matt DeVillier <matt.devillier@gmail.com>
2016-10-15 20:03:47 -0500

    drivers/intel/gma: prevent Linux from using broken ACPI backlight
    By default, linux prioritizes the ACPI backlight controls over the Intel
    driver ones, which causes them to be non-functional on HSW/BDW devices.
    This patch strips out just enough so that Linux will use the functional
    driver controls, while not breaking Windows and OS X functionality
    
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>

--- src/drivers/intel/gma/acpi.c
+++ src/drivers/intel/gma/acpi.c
@@ -68,7 +68,7 @@ drivers_intel_gma_displays_ssdt_generate(const struct i915_gpu_controller_info *
 			acpigen_emit_byte(0xa4); /* ReturnOp.  */
 			acpigen_emit_namestring("^^XBCL");
 			acpigen_pop_len();
-
+#if !IS_ENABLED(CONFIG_CPU_INTEL_HASWELL) && !IS_ENABLED(CONFIG_SOC_INTEL_BROADWELL)
 			/*
 			  Method (_BCM, 1, NotSerialized)
 			  {
@@ -79,7 +79,7 @@ drivers_intel_gma_displays_ssdt_generate(const struct i915_gpu_controller_info *
 			acpigen_emit_namestring("^^XBCM");
 			acpigen_emit_byte(0x68); /* Arg0Op.  */
 			acpigen_pop_len();
-
+#endif
 			/*
 			  Method (_BQC, 0, NotSerialized)
 			  {
