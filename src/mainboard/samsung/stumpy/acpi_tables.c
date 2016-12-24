2d524092e8359d16c52e5fc4415b227df515ff11
Matt DeVillier <matt.devillier@gmail.com>
2016-12-24 15:33:29 -0600

    samsung/stumpy: adjust fan control
    use same default fan speed, fan control as other IT8772 devices
    
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>

--- src/mainboard/samsung/stumpy/acpi_tables.c
+++ src/mainboard/samsung/stumpy/acpi_tables.c
@@ -19,8 +19,6 @@ void mainboard_fill_gnvs(struct global_nvs *gnvs)
 	gnvs->s5u0 = 1;
 	gnvs->s5u1 = 1;
 
-	gnvs->f4of = FAN4_THRESHOLD_OFF;
-	gnvs->f4on = FAN4_THRESHOLD_ON;
 	gnvs->f4pw = FAN4_PWM;
 
 	gnvs->f3of = FAN3_THRESHOLD_OFF;
