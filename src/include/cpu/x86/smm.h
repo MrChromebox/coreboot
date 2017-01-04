84526c99505b8133c9fd181563e09f368431c8ef
Matt DeVillier <matt.devillier@gmail.com>
2017-01-03 20:02:43 -0600

    soc/baytrail: use SMM to toggle ACPI/PCI mode for eMMC
    Add one SMM method and modify an existing one to allow payload
    (Tianocore) to toggle the eMMC from ACPI mode to PCI mode in
    order to enable use as boot device, then back to ACPI mode
    before the OS loads
    
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>

--- src/include/cpu/x86/smm.h
+++ src/include/cpu/x86/smm.h
@@ -37,6 +37,7 @@
 #define APM_CNT_GNVS_UPDATE	0xea
 #define APM_CNT_FINALIZE	0xcb
 #define APM_CNT_LEGACY		0xcc
+#define APM_CNT_END_OF_DXE	0xcd
 #define APM_CNT_SMMSTORE	0xed
 #define APM_CNT_ELOG_GSMI	0xef
 #define APM_STS		0xb3
