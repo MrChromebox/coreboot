c47617da7e5d01a3fb651821459068317fc67bbb
Matt DeVillier <matt.devillier@gmail.com>
2017-06-19 22:52:52 -0500

    drivers/intel/gma: Set header pcon value
    Windows drivers require this to be set
    
    Change-Id: I80b12f4dcd91529cc8fa3ff780cd13ac7a8045e4
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>

--- src/drivers/intel/gma/opregion.c
+++ src/drivers/intel/gma/opregion.c
@@ -371,7 +371,10 @@ enum cb_err intel_gma_init_igd_opregion(void)
 	// TODO Initialize Mailbox 1
 	opregion->mailbox1.clid = 1;
 
-	// TODO Initialize Mailbox 3
+	//From Intel OpRegion reference doc
+	opregion->header.pcon = 279;
+
+	// Initialize Mailbox 3
 	opregion->mailbox3.bclp = IGD_BACKLIGHT_BRIGHTNESS;
 	opregion->mailbox3.pfit = IGD_FIELD_VALID | IGD_PFIT_STRETCH;
 	opregion->mailbox3.pcft = 0; // should be (IMON << 1) & 0x3e
