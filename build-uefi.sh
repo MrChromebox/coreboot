e18c990f59a56774d4437852d307a84f09b4a071
Matt DeVillier <matt.devillier@gmail.com>
2018-02-03 00:22:06 -0600

    build: add configs, build scripts, cfbs files for supported boards
    Change-Id: I9ba1b3dc448cc447ab83d64f08d14406826fcede
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>

---
+++ build-uefi.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+#
+
+set -e
+
+platforms=('snb_ivb' 'hsw' 'byt' 'bdw' 'bsw' 'skl' 'apl' 'kbl' 'whl' 'glk' \
+           'cml' 'jsl' 'tgl' 'adl' 'adl_n' 'mtl' 'str' 'pco' 'czn' 'mdn')
+build_targets=()
+debug_mode=false
+
+output_folder="../roms"
+mkdir -p ${output_folder}
+
+# Parse command line arguments
+for arg in "$@"; do
+	if [ "$arg" = "--debug" ]; then
+		debug_mode=true
+	else
+		build_targets+=("$arg")
+	fi
+done
+
+# If no build targets specified, build all configs
+if [ ${#build_targets[@]} -eq 0 ]; then
+	for subdir in "${platforms[@]}"; do
+		for cfg in configs/"$subdir"/config*.*; do
+			build_targets+=("$(basename "$cfg" | cut -f2 -d'.')")
+		done
+	done
+fi
+
+# get git rev
+rev=$(git describe --tags --dirty)
+
+for device in "${build_targets[@]}"; do
+	if [ "$debug_mode" = true ]; then
+		filename="coreboot_edk2-${device}-mrchromebox_debug_$(date +"%Y%m%d").rom"
+	else
+		filename="coreboot_edk2-${device}-mrchromebox_$(date +"%Y%m%d").rom"
+	fi
+	rm -f ${output_folder}/"${filename}"*
+	rm -rf ./build
+	cfg_file=$(find ./configs -name "config.$device.uefi")
+	cp "$cfg_file" .config
+	echo "CONFIG_LOCALVERSION=\"${rev}\"" >> .config
+	if [ "$debug_mode" = true ]; then
+		echo "CONFIG_CONSOLE_SERIAL=y" >> .config
+		echo "CONFIG_EDK2_DEBUG=y" >> .config
+	fi
+	make clean
+	make olddefconfig
+	if ! make -j"$(nproc)"; then
+		echo -e "Error building $device"
+		exit 1
+	fi
+	cp ./build/coreboot.rom ./"${filename}"
+	sha1sum "${filename}" > "${filename}.sha1"
+	mv "${filename}"* "${output_folder}"
+done
