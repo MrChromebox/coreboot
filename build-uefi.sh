55d3d0a5bceaeed3357ad1f46688fcaa6b928878
Matt DeVillier <matt.devillier@gmail.com>
2018-02-03 00:22:06 -0600

    build: add configs, build scripts, cfbs files for supported boards
    currently supported boards:
    
    snb/ivb: butterfly, link, lumpy, parrot, stout, stumpy
    hsw: falco, leon, mccloud, monroe, panther, peppy, tricky, wolf
    bdw: auron_paine, auron_yuna, buddy, gandof, guado, lulu, rikku,
         samus, tidus
    byt: banjo, candy, clapper, enguarde, glimmer, gnawty, heli, kip,
         ninja, orco, quawks, squawks, sumo, swanky, winky
    bsw: banon, celes, cyan, edgar, kefka, reks, relm,
         setzer, terra, ultima, wizpig
    skl: asuka, caroline, cave, chell, lars, sentry, librem13v2
    kbl: eve, fizz
    
    Change-Id: I9ba1b3dc448cc447ab83d64f08d14406826fcede
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>

---
+++ build-uefi.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+#
+
+set -e
+
+snb_ivb=("butterfly" "link" "lumpy" "parrot" "stout" "stumpy")
+hsw=("falco" "leon" "mccloud" "monroe" "panther" "peppy" "tricky" "wolf" \
+	"zako");
+bdw=("auron_paine" "auron_yuna" "buddy" "gandof" "guado" "lulu" "rikku" \
+	"samus" "tidus");
+byt=("banjo" "candy" "clapper" "enguarde" "glimmer" "gnawty" "heli" \
+	"kip" "ninja" "orco" "quawks" "squawks" "sumo" "swanky" "winky");
+bsw=("banon" "celes" "cyan" "edgar" "kefka" "reks" "relm" \
+	"setzer" "terra" "ultima" "wizpig");
+skl=("asuka" "caroline" "cave" "chell" "lars" "sentry")
+kbl=("eve" "fizz")
+
+if [ -z "$1" ]; then
+	build_targets=($(printf "%s " "${hsw[@]}" "${bdw[@]}" "${byt[@]}" "${snb_ivb[@]}" "${bsw[@]}" "${skl[@]}" "${kbl[@]}"));
+else
+	build_targets=($@)
+fi
+
+for device in ${build_targets[@]}
+do
+	filename="coreboot_tiano-${device}-mrchromebox_`date +"%Y%m%d"`.rom"
+	rm -f ~/dev/firmware/${filename}*
+	rm -rf ./build
+	cp configs/.config.${device}.uefi .config
+	make clean
+	make olddefconfig
+	make
+	cp ./build/coreboot.rom ./${filename}
+	cbfstool ${filename} print
+	sha1sum ${filename} > ${filename}.sha1
+	mv ${filename}* ~/dev/firmware/
+	if [ "${device}" == "peppy" ]; then
+		filename="coreboot_tiano-${device}_elan-mrchromebox_`date +"%Y%m%d"`.rom"
+		rm -rf ./build
+		sed -i 's/# CONFIG_ELAN_TRACKPAD_ACPI is not set/CONFIG_ELAN_TRACKPAD_ACPI=y/' .config
+		make
+		cp ./build/coreboot.rom ./${filename}
+		cbfstool ${filename} print
+		sha1sum ${filename} > ${filename}.sha1
+		mv ${filename}* ~/dev/firmware/
+	fi
+done
