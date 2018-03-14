7e8a212e49dc101f626a74fe4be4c930b013ee65
Matt DeVillier <matt.devillier@gmail.com>
2018-03-14 12:07:40 -0500

    drivers/intel/gma: Inject GOP driver version into OpRegion ACPI
    If a GOP driver version is specified, inject that into
    the OpRegion ACPI DVER (GOP version) field.
    
    Change-Id: Ica031f79cb884c0b54e0acca6633694dbfd38311
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>

--- src/drivers/intel/gma/opregion.c
+++ src/drivers/intel/gma/opregion.c
@@ -283,6 +283,13 @@ enum cb_err intel_gma_init_igd_opregion(void)
 		sizeof(opregion->header.signature));
 	memcpy(opregion->header.vbios_version, vbt->coreblock_biosbuild,
 					ARRAY_SIZE(vbt->coreblock_biosbuild));
+
+	/* Inject GOP driver version */
+#if CONFIG(SET_GOP_DRIVER_VERSION)
+	memcpy(opregion->header.dver, STR16(CONFIG_GOP_DRIVER_VERSION),
+			2 * strlen(CONFIG_GOP_DRIVER_VERSION));
+#endif
+
 	/* Extended VBT support */
 	if (vbt->hdr_vbt_size > sizeof(opregion->vbt.gvd1)) {
 		ext_vbt = cbmem_add(CBMEM_ID_EXT_VBT, vbt->hdr_vbt_size);
