eb6b08ac240bf0b914795eb9a517fe50f470c804
Harsha Priya <harhapriya.n@intel.com>
2018-05-07 16:17:32 -0700

    lib/nhlt: Change NHLT to comply to the latest spec
    This patch changes NHLT to comply to the latest spec as follows:
    
    1. Maintain SSP endpoint InstanceId as 0
    2. Add capability_size parameter at the end of the nhlt
    3. Add more config_type enum values to accomodate feedback stream
    4. Program virtual_slot values for max98373, max98927,
       and rt5514 nhlt files
    5. Add NHLT feedback_config parameters
    
    Change-Id: Ib13110f4d42e140aa1356ed0b6a6127943b6e800
    Signed-off-by: Harsha Priya <harshapriya.n@intel.com>

--- src/soc/intel/skylake/nhlt/max98373.c
+++ src/soc/intel/skylake/nhlt/max98373.c
@@ -3,15 +3,6 @@
 #include <soc/nhlt.h>
 
 static const struct nhlt_format_config max98373_render_formats[] = {
-	/* 48 KHz 24-bits per sample. */
-	{
-		.num_channels = 2,
-		.sample_freq_khz = 48,
-		.container_bits_per_sample = 32,
-		.valid_bits_per_sample = 24,
-		.speaker_mask = SPEAKER_FRONT_LEFT | SPEAKER_FRONT_RIGHT,
-		.settings_file = "max98373-render-2ch-48khz-24b.bin",
-	},
 	/* 48 KHz 16-bits per sample. */
 	{
 		.num_channels = 2,
@@ -44,6 +35,26 @@ static const struct nhlt_format_config max98373_capture_formats[] = {
 	},
 };
 
+static struct nhlt_feedback_config render_config = {
+	.tdm_config = {
+		.virtual_slot = 0x0,
+		.config_type = NHLT_TDM_RENDER_FEEDBACK,
+	},
+	.feedback_virtual_slot = 2,
+	.feedback_channels = 4,
+	.feedback_valid_bits_per_sample = 16,
+};
+
+static struct nhlt_feedback_config capture_config = {
+	.tdm_config = {
+		.virtual_slot = 0x2,
+		.config_type = NHLT_TDM_RENDER_FEEDBACK,
+	},
+	.feedback_virtual_slot = 0,
+	.feedback_channels = 2,
+	.feedback_valid_bits_per_sample = 16,
+};
+
 static const struct nhlt_endp_descriptor max98373_descriptors[] = {
 	{
 		.link = NHLT_LINK_SSP,
@@ -51,6 +62,8 @@ static const struct nhlt_endp_descriptor max98373_descriptors[] = {
 		.direction = NHLT_DIR_RENDER,
 		.vid = NHLT_VID,
 		.did = NHLT_DID_SSP,
+		.cfg = &render_config,
+		.cfg_size = sizeof(render_config),
 		.formats = max98373_render_formats,
 		.num_formats = ARRAY_SIZE(max98373_render_formats),
 	},
@@ -60,6 +73,8 @@ static const struct nhlt_endp_descriptor max98373_descriptors[] = {
 		.direction = NHLT_DIR_CAPTURE,
 		.vid = NHLT_VID,
 		.did = NHLT_DID_SSP,
+		.cfg = &capture_config,
+		.cfg_size = sizeof(capture_config),
 		.formats = max98373_capture_formats,
 		.num_formats = ARRAY_SIZE(max98373_capture_formats),
 	},
