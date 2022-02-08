e0a2c66066eef3309e49b9870ecc4480795ab62b
Matt DeVillier <matt.devillier@gmail.com>
2022-02-07 18:59:30 -0600

    ec/chromeec/ec_lpc: increase cmd timeout to 3s, retry to 5us
    Change-Id: I7f16f17273b4687ada653c3c160879e048f34b4a
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>

--- src/ec/google/chromeec/ec_lpc.c
+++ src/ec/google/chromeec/ec_lpc.c
@@ -94,10 +94,10 @@ static inline u8 write_byte(u8 val, u16 port)
 static int google_chromeec_status_check(u16 port, u8 mask, u8 cond)
 {
 	struct stopwatch timeout_sw;
-	/* One second is more than plenty for any EC operation to complete */
-	const uint64_t ec_status_timeout_us = 1 * USECS_PER_SEC;
-	/* Wait 1 usec between read attempts  */
-	const uint64_t ec_status_read_period_us = 1;
+	/* Three seconds is more than plenty for any EC operation to complete */
+	const uint64_t ec_status_timeout_us = 3 * USECS_PER_SEC;
+	/* Wait 5 usec between read attempts  */
+	const uint64_t ec_status_read_period_us = 5;
 
 	stopwatch_init_usecs_expire(&timeout_sw, ec_status_timeout_us);
 	do {
