233f27c9e28335c244b0efa09fd8456df0b13578
Matt DeVillier <matt.devillier@gmail.com>
2022-03-03 17:17:46 -0600

    rework configs/build script
    Change-Id: Ia064fcc5c66c8d7a709a0bd708c1077285d52f85
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>

--- build-uefi.sh
+++ build-uefi.sh
@@ -3,43 +3,32 @@
 
 set -e
 
-snb_ivb=('butterfly' 'link' 'lumpy' 'parrot' 'stout' 'stumpy')
-hsw=('falco' 'leon' 'mccloud' 'monroe' 'panther' 'peppy' 'tricky' 'wolf' \
-	'zako');
-bdw=('auron_paine' 'auron_yuna' 'buddy' 'gandof' 'guado' \
-     'lulu' 'rikku' 'samus' 'tidus');
-byt=('banjo' 'candy' 'clapper' 'enguarde' 'glimmer' 'gnawty' 'heli' \
-	'kip' 'ninja' 'orco' 'quawks' 'squawks' 'sumo' 'swanky' 'winky');
-bsw=('banon' 'celes' 'cyan' 'edgar' 'kefka' 'reks' 'relm' \
-	'setzer' 'terra' 'ultima' 'wizpig');
-skl=('asuka' 'caroline' 'cave' 'chell' 'lars'  'sentry')
-kbl=('atlas' 'eve' 'fizz' 'nami' 'nautilus' 'nocturne' 'rammus' 'soraka')
-str=('aleena' 'barla' 'careena' 'kasumi' 'liara' 'treeya')
-cml=('akemi' 'dragonair' 'dratini' 'duffy' 'faffy' 'helios' 'jinlon' 'kaisa' \
-     'kindred' 'kled' 'kohaku' 'nightfury' 'noibat' 'wyvern')
-glk=('ampton' 'bloog' 'blooguard' 'blooglet' 'bluebird' 'casta')
-zen2=('morphius')
-purism=('librem_13v1' 'librem_15v2' 'librem_13v2' 'librem_15v3' 'librem_13v4' \
-        'librem_15v4' 'librem_mini' 'librem_mini_v2' 'librem_14')
+platforms=('snb_ivb' 'hsw' 'byt' 'bdw' 'bsw' 'skl' 'apl' 'kbl' 'whl' 'glk' \
+           'cml' 'jsl' 'tgl' 'str' 'zen2')
+build_targets=()
 
 json_file=cbmodels.json
 rom_path=https://www.mrchromebox.tech/files/firmware/full_rom/
 echo -e "{" > $json_file
 
 if [ -z "$1" ]; then
-	build_targets=($(printf "%s " "${snb_ivb[@]}" "${hsw[@]}" "${bdw[@]}" \
-		"${byt[@]}" "${bsw[@]}" "${skl[@]}" "${kbl[@]}" "${str[@]}" \
-		"${cml[@]}" "${glk[@]}" "${zen2[@]}" "${purism[@]}"));
+	for subdir in "${platforms[@]}"; do
+		for cfg in configs/$subdir/config*.*; do
+			build_targets+=("$(basename $cfg | cut -f2 -d'.')")
+		done
+	done
 else
 	build_targets=($@)
 fi
 
-for device in ${build_targets[@]}
-do
+echo -e "\n\nBuilding for targets: ${build_targets[@]}\n\n"
+
+for device in "${build_targets[@]}"; do
 	filename="coreboot_tiano-${device}-mrchromebox_$(date +"%Y%m%d").rom"
 	rm -f ~/dev/firmware/${filename}*
 	rm -rf ./build
-	cp configs/.config.${device}.uefi .config
+	cfg_file=$(find ./configs -name "config.$device.uefi")
+	cp "$cfg_file" .config
 	make clean
 	make olddefconfig
 	make -j$(nproc)
