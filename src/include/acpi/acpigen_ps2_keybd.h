db481d120cead4666f92f0bc4b351061b0e8ef37
Matt DeVillier <matt.devillier@gmail.com>
2022-12-23 16:14:39 -0600

    ec/google/chromeec: Add copy of vivaldi mapping under CKSC
    Generate a second copy of the physmap only, to allow coolstar's Windows
    drivers to properly map top row keys on devices with vivaldi support.
    
    Change-Id: I5f0e06b562511481a57aa0895986d441dba326fd
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>

--- src/include/acpi/acpigen_ps2_keybd.h
+++ src/include/acpi/acpigen_ps2_keybd.h
@@ -36,6 +36,7 @@ void acpigen_ps2_keyboard_dsd(const char *scope, uint8_t num_top_row_keys,
 			      enum ps2_action_key action_keys[],
 			      bool can_send_function_keys,
 			      bool has_numeric_keypad, bool has_scrnlock_key,
-			      bool has_alpha_num_punct_keys);
+			      bool has_alpha_num_punct_keys,
+			      bool generate_keymap);
 
 #endif /* __ACPI_ACPIGEN_PS2_KEYBD_H__ */
