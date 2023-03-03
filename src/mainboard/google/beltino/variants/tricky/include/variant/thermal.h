03a6e0614c29d3a8eeeb8e7c982117c112bde0a2
Matt DeVillier <matt.devillier@gmail.com>
2023-03-03 10:20:30 -0600

    mb/google/beltino/tricky: update fan curve
    Change-Id: I67d052fde9f8f42fc1ec14ca9974ac94c16b48f7
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>

--- src/mainboard/google/beltino/variants/tricky/include/variant/thermal.h
+++ src/mainboard/google/beltino/variants/tricky/include/variant/thermal.h
@@ -3,28 +3,28 @@
 #ifndef THERMAL_H
 #define THERMAL_H
 
-/* Fan is at default speed */
-#define FAN4_PWM		0x47
+/* Fan is at default speed: ~2600 rpm */
+#define FAN4_PWM		0x4b
 
-/* Fan is at LOW speed */
-#define FAN3_THRESHOLD_OFF	50
-#define FAN3_THRESHOLD_ON	60
-#define FAN3_PWM		0x62
+/* Fan is at LOW speed: ~3000 rpm */
+#define FAN3_THRESHOLD_OFF	30
+#define FAN3_THRESHOLD_ON	40
+#define FAN3_PWM		0x5B
 
-/* Fan is at MEDIUM speed */
-#define FAN2_THRESHOLD_OFF	65
-#define FAN2_THRESHOLD_ON	77
-#define FAN2_PWM		0x86
+/* Fan is at MEDIUM speed: ~4000 rpm */
+#define FAN2_THRESHOLD_OFF	40
+#define FAN2_THRESHOLD_ON	50
+#define FAN2_PWM		0x87
 
-/* Fan is at HIGH speed */
-#define FAN1_THRESHOLD_OFF	77
-#define FAN1_THRESHOLD_ON	85
-#define FAN1_PWM		0xa8
+/* Fan is at HIGH speed: ~5000 rpm */
+#define FAN1_THRESHOLD_OFF	50
+#define FAN1_THRESHOLD_ON	60
+#define FAN1_PWM		0xc3
 
-/* Fan is at FULL speed */
-#define FAN0_THRESHOLD_OFF	85
-#define FAN0_THRESHOLD_ON	90
-#define FAN0_PWM		0xdc
+/* Fan is at FULL speed: ~6000 rpm */
+#define FAN0_THRESHOLD_OFF	60
+#define FAN0_THRESHOLD_ON	70
+#define FAN0_PWM		0xff
 
 /* Temperature which OS will shutdown at */
 #define CRITICAL_TEMPERATURE	98
