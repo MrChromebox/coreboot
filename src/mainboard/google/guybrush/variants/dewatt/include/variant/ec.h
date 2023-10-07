31cc343d4ce13b7e18f1a347c9900fdbbb3c43e1
Matt DeVillier <matt.devillier@gmail.com>
2023-10-06 19:34:38 -0500

    mb/google/guybrush/var/dewatt: Add non-ChromeOS TBMC support
    The TBMC ACPI device is used by Windows ChromeEC drivers to determine
    tablet mode and to enable motion sensors (accelerometer, gyroscope).
    Since it's not needed/used by ChromeOS, restrict its inclusion to
    non-ChromeOS builds.
    
    TEST=build/boot Win11/Linux on dewatt, verify tablet mode and rotation
    work properly, keyboard/touchpad disabled in tablet mode.
    
    Change-Id: I3eeae7b453589a2253226709dd6cfcff1862ea17
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>

--- src/mainboard/google/guybrush/variants/dewatt/include/variant/ec.h
+++ src/mainboard/google/guybrush/variants/dewatt/include/variant/ec.h
@@ -1,3 +1,13 @@
 /* SPDX-License-Identifier: GPL-2.0-or-later */
 
+#ifndef __VARIANT_EC_H__
+#define __VARIANT_EC_H__
+
 #include <baseboard/ec.h>
+
+/* Enable Tablet switch for Windows drivers */
+#if !CONFIG(CHROMEOS)
+#define EC_ENABLE_TBMC_DEVICE
+#endif
+
+#endif
