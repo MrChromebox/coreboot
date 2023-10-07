c401c7ee087a88559e73d15cb78445d15166f353
Matt DeVillier <matt.devillier@gmail.com>
2023-10-06 19:34:22 -0500

    mb/google/skyrim/frostflow: Add TBMC support
    Change-Id: Iedc68797776d43f37dd97e5251cf9b9a016f1bd5
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>

--- src/mainboard/google/skyrim/variants/frostflow/include/variant/ec.h
+++ src/mainboard/google/skyrim/variants/frostflow/include/variant/ec.h
@@ -1,3 +1,11 @@
 /* SPDX-License-Identifier: GPL-2.0-or-later */
 
+#ifndef __VARIANT_EC_H__
+#define __VARIANT_EC_H__
+
 #include <baseboard/ec.h>
+
+/* Enable Tablet switch */
+#define EC_ENABLE_TBMC_DEVICE
+
+#endif
