822782a100162752353afe93315be0f43ce1e46b
Matt DeVillier <matt.devillier@gmail.com>
2024-02-13 11:47:11 -0600

    build-uefi.sh: drop creation of json file
    Change-Id: I9729f11c9885834469860bd2b856bc7e4a4157ab
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>

--- build-uefi.sh
+++ build-uefi.sh
@@ -10,10 +10,6 @@ build_targets=()
 output_folder="../roms"
 mkdir -p ${output_folder}
 
-json_file=cbmodels.json
-rom_path=https://www.mrchromebox.tech/files/firmware/full_rom/
-echo -e "{" > $json_file
-
 if [ -z "$1" ]; then
 	for subdir in "${platforms[@]}"; do
 		for cfg in configs/$subdir/config*.*; do
@@ -39,12 +35,5 @@ for device in "${build_targets[@]}"; do
 	make -j$(nproc)
 	cp ./build/coreboot.rom ./${filename}
 	sha1sum ${filename} > ${filename}.sha1
-	echo -e "\t\"${device}\": {" >> $json_file
-	echo -e "\t\t\"url\": \"${rom_path}${filename}\"," >> $json_file
-	echo -e "\t\t\"sha1\": \"$(cat ${filename}.sha1 | awk 'NR==1{print $1}')\"" >> $json_file
-	echo -e "\t}," >> $json_file
 	mv ${filename}* ${output_folder}
 done
-echo -e "}" >> $json_file
-# remove last comma
-sed -i 's/\(.*\),/\1/' $json_file
