110202913c9ac17241a89a685a6c58b73d4f6a98
Matt DeVillier <matt.devillier@gmail.com>
2024-06-10 00:06:59 -0500

    ec/google/chromeec: Implement EC software sync V2
    Change-Id: I3f1033994d761bb5a957af3d0e219dbcf0d58062
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>

--- src/ec/google/chromeec/ec.c
+++ src/ec/google/chromeec/ec.c
@@ -1288,6 +1288,9 @@ void google_chromeec_init(void)
 {
 	google_chromeec_log_uptimeinfo();
 
+	/* Run EC sync */
+	google_chromeec_swsync();
+
 	/* Enable automatic fan control */
 	if (CONFIG(EC_GOOGLE_CHROMEEC_AUTO_FAN_CTRL)) {
 		ec_cmd_thermal_auto_fan_ctrl(PLAT_EC);
