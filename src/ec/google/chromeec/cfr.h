f54249ecfe394f1a7b08f6f3d58c512aa896eea9
Matt DeVillier <matt.devillier@gmail.com>
2025-04-22 16:19:59 -0500

    ec/google/chromeec: Add CFR objects for setup options
    Add a header with CFR objects for existing configuration options,
    so that supported boards can make use of them without duplication.
    
    Change-Id: I1a9e59885afad72c371a707ac7de177f3e807704
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>

---
+++ src/ec/google/chromeec/cfr.h
@@ -0,0 +1,28 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+/*
+ * CFR enums and structs which are used to control EC settings.
+ */
+
+#ifndef CHROMEEC_CFR_H
+#define CHROMEEC_CFR_H
+
+#include <drivers/option/cfr_frontend.h>
+#include "ec.h"
+
+static const struct sm_object ec_sw_sync = SM_DECLARE_BOOL({
+	.opt_name	= "ec_sw_sync",
+	.ui_name	= "EC Software Sync",
+	.ui_helptext	= "Enable or disable updating of EC-RW firmware",
+	.default_value	= true,
+});
+
+static const struct sm_object ec_rw_jump = SM_DECLARE_BOOL({
+	.opt_name	= "ec_rw_jump",
+	.ui_name	= "Use EC-RW Firmware",
+	.ui_helptext	= "Jump to EC-RW firmware after EC SW Sync (if enabled). "
+			  "If disabled, EC-RO firmware will be used instead.",
+	.default_value	= true,
+});
+
+#endif /* CHROMEEC_CFR_H */
