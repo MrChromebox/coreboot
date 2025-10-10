b80d8c72bd99bbc8fbc85e5af63c074cf742cbfc
Matt DeVillier <matt.devillier@gmail.com>
2025-10-10 17:20:26 -0500

    mb/google/brya: Add CFR storage option for TANIKS/TAEKO
    TODO: cleaner implementation
    
    Change-Id: I90efb03e01efe63c0d2b4ccbbc3a1d00f55b3363
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>

--- src/mainboard/google/brya/variants/taniks/variant.c
+++ src/mainboard/google/brya/variants/taniks/variant.c
@@ -1,8 +1,39 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
+#include <baseboard/variants.h>
+#include <device/device.h>
+#include <option.h>
 #include <sar.h>
+#include <types.h>
+
+enum storage_device {
+	STORAGE_NVME = 0,
+	STORAGE_EMMC = 1,
+};
 
 const char *get_wifi_sar_cbfs_filename(void)
 {
 	return "wifi_sar_0.hex";
+}
+
+void variant_devtree_update(void)
+{
+	/* Get device references for both storage options */
+	struct device *nvme_dev = pcidev_on_root(0x06, 0);  /* CPU PCIe RP1 for NVMe */
+	struct device *emmc_dev = pcidev_on_root(0x1d, 0);  /* PCH PCIe RP9 for eMMC */
+
+	/* Get the selected storage device from CMOS */
+	uint8_t storage_selection = get_uint_option("storage_device", STORAGE_NVME);
+
+	/* Disable the non-selected storage device */
+	switch (storage_selection) {
+	case STORAGE_NVME:
+		if (emmc_dev)
+			emmc_dev->enabled = 0;
+		break;
+	case STORAGE_EMMC:
+		if (nvme_dev)
+			nvme_dev->enabled = 0;
+		break;
+	}
 }
