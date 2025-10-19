8d488be64bb03cff009654bf9f3c8647dd500306
Matt DeVillier <matt.devillier@gmail.com>
2025-10-19 17:33:46 -0500

    ec/google/chromeec: Add CFR option for fan control
    Change-Id: Icef1364b1242777527c32e94b503a755183f0379
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>

--- src/ec/google/chromeec/cfr.h
+++ src/ec/google/chromeec/cfr.h
@@ -27,6 +27,26 @@ static const struct sm_object ec_rw_jump = SM_DECLARE_BOOL({
 	.default_value	= true,
 });
 
+static void update_fan_control(const struct sm_object *obj, struct sm_object *new)
+{
+	if (!CONFIG(EC_GOOGLE_CHROMEEC_AUTO_FAN_CTRL) && !google_chromeec_has_fan()) {
+		new->sm_bool.flags = CFR_OPTFLAG_SUPPRESS;
+		new->sm_bool.default_value = 0;
+	}
+}
+
+static const struct sm_object auto_fan_control = SM_DECLARE_ENUM({
+	.opt_name       = "auto_fan_control",
+	.ui_name        = "Automatic Fan Control",
+	.ui_helptext    = "Enable or disable automatic fan control.",
+	.default_value  = CONFIG(EC_GOOGLE_CHROMEEC_AUTO_FAN_CTRL),
+	.values         = (struct sm_enum_value[]) {
+		{ "Enabled", 1 },
+		{ "Disabled", 0 },
+		SM_ENUM_VALUE_END,
+	},
+}, WITH_CALLBACK(update_fan_control));
+
 static const struct sm_enum_value ec_backlight_values[] = {
 	{ "0%", 0 },
 	{ "25%", 25 },
