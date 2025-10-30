4df53efc0ce1767042932013b9abb165338dbf63
Matt DeVillier <matt.devillier@gmail.com>
2025-10-30 09:33:02 -0500

    ec/google/chromeec: Add CFR objects for EC Sync, RW jump options
    Change-Id: I1a9e59885afad72c371a707ac7de177f3e807704
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>

--- src/ec/google/chromeec/cfr.h
+++ src/ec/google/chromeec/cfr.h
@@ -76,4 +76,21 @@ static const struct sm_object ec_rgb_kb_color = SM_DECLARE_ENUM({
 	.values         = ec_rgb_backlight_values,
 }, WITH_CALLBACK(update_rgb_kb_backlight));
 
+static const struct sm_object ec_sw_sync = SM_DECLARE_BOOL({
+	.opt_name	= "ec_sw_sync",
+	.ui_name	= "EC Software Sync",
+	.ui_helptext	= "Enable or disable updating of EC-RW firmware",
+	.default_value	= true,
+});
+
+static const struct sm_object ec_rw_jump = SM_DECLARE_BOOL({
+	.opt_name	= "ec_rw_jump",
+	.ui_name	= "Use EC-RW Firmware",
+	.ui_helptext	= "Jump to EC-RW firmware after EC SW Sync. "
+			  "Warning: Disabling EC-RW may result in undefined behavior, ranging"
+			  "from USB-C/TBT accessories not working to the keyboard not working."
+			  "If disabled, EC-RO firmware will be used instead.",
+	.default_value	= true,
+});
+
 #endif /* _CHROMEEC_CFR_H_ */
