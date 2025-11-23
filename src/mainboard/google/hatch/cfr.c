61c6bbe0e7c9d622ba247b6d13ab3a0ed11b084a
Matt DeVillier <matt.devillier@gmail.com>
2025-11-23 14:14:25 -0600

    mb/google: Set s0ix_enable false for pre-TGL platforms
    Change-Id: I2c5019f4dd15aadc7c853af0bc3aa79c7b590587
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>

--- src/mainboard/google/hatch/cfr.c
+++ src/mainboard/google/hatch/cfr.c
@@ -5,6 +5,12 @@
 #include <ec/google/chromeec/cfr.h>
 #include <intelblocks/cfr.h>
 #include <soc/cfr.h>
+#include <stdbool.h>
+
+static const struct cfr_default_override cfr_overrides[] = {
+	CFR_OVERRIDE_BOOL("s0ix_enable", false),
+	CFR_OVERRIDE_END,
+};
 
 static const struct sm_object touchpad_wake = SM_DECLARE_BOOL({
 	.opt_name	= "touchpad_wake",
@@ -55,5 +61,6 @@ static struct sm_obj_form *sm_root[] = {
 
 void mb_cfr_setup_menu(struct lb_cfr *cfr_root)
 {
+	cfr_register_overrides(cfr_overrides);
 	cfr_write_setup_menu(cfr_root, sm_root);
 }
