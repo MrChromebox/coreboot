01e070a49dce400393acfa342a7d59689f9270ec
Matt DeVillier <matt.devillier@gmail.com>
2025-12-14 18:54:49 -0600

    Revert "mb/google/brya: Add CFR storage option for TANIKS/TAEKO"
    This reverts commit b80d8c72bd99bbc8fbc85e5af63c074cf742cbfc.

--- src/mainboard/google/brya/variants/baseboard/brya/cfr.c
+++ src/mainboard/google/brya/variants/baseboard/brya/cfr.c
@@ -7,32 +7,6 @@
 #include <intelblocks/cfr.h>
 #include <soc/cfr.h>
 
-#if CONFIG(BOARD_GOOGLE_TANIKS) || CONFIG(BOARD_GOOGLE_TAEKO)
-
-enum storage_device {
-	STORAGE_NVME = 0,
-	STORAGE_EMMC = 1,
-};
-
-static const struct sm_object storage_device_opt = SM_DECLARE_ENUM({
-	.opt_name	= "storage_device",
-	.ui_name	= "Storage Device",
-	.ui_helptext	= "Select which storage device to use (NVMe SSD or eMMC)",
-	.default_value	= STORAGE_NVME,
-	.values		= (const struct sm_enum_value[]) {
-				{ "NVMe SSD",		STORAGE_NVME	},
-				{ "eMMC",		STORAGE_EMMC	},
-				SM_ENUM_VALUE_END			},
-});
-static struct sm_obj_form devices = {
-	.ui_name = "Devices",
-	.obj_list = (const struct sm_object *[]) {
-		&storage_device_opt,
-		NULL
-	},
-};
-#endif
-
 static struct sm_obj_form system = {
 	.ui_name = "System",
 	.obj_list = (const struct sm_object *[]) {
@@ -66,9 +40,6 @@ static struct sm_obj_form ec = {
 
 static struct sm_obj_form *sm_root[] = {
 	&system,
-#if CONFIG(BOARD_GOOGLE_TANIKS) || CONFIG(BOARD_GOOGLE_TAEKO)
-	&devices,
-#endif
 	&ec,
 	NULL
 };
