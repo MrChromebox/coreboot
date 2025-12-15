43277745fb44c9d13ac6218b50e21d90ef6db6bf
Matt DeVillier <matt.devillier@gmail.com>
2025-12-14 18:13:21 -0600

    mb/google/brya: Add CFR-based storage selection for taeko/taniks
    Add support for selecting NVMe or eMMC storage via CFR option on
    taeko and taniks variants. Override fw_config_probe() to check the
    CFR "storage_device" option and enable/disable the appropriate PCIe
    root port based on user selection.
    
    This allows runtime configuration of storage devices while ensuring
    only the selected device is initialized, saving power and resources.
    
    Change-Id: Ic555f93763736adb5837534b8011aa9c123fea08
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>

--- src/mainboard/google/brya/variants/taeko/Makefile.mk
+++ src/mainboard/google/brya/variants/taeko/Makefile.mk
@@ -8,6 +8,7 @@ romstage-y += memory.c
 
 ramstage-y += gpio.c
 
+romstage-$(CONFIG_FW_CONFIG) += fw_config.c
 ramstage-$(CONFIG_FW_CONFIG) += fw_config.c
 
 ramstage-$(CONFIG_FW_CONFIG) += variant.c
