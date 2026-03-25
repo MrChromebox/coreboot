1d97799db60db498ff467f5879ff698568e5e57e
Matt DeVillier <matt.devillier@gmail.com>
2026-03-25 16:40:55 -0500

    mb/google/jecht: Add CFR options for CPU undervolt
    Use the newly-added OC_MAILBOX driver and expose CFR options to apply
    per-plane undervolt.
    
    TEST=build/boot Guado, verify voltage adjustments in CFR reflected in
    Windows Throttlestop app.
    
    Change-Id: I51b842c17a97320b58c0f1c9e100e759b58d02d3
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>

--- src/mainboard/google/jecht/cfr.c
+++ src/mainboard/google/jecht/cfr.c
@@ -2,6 +2,7 @@
 
 #include <boot/coreboot_tables.h>
 #include <cpu/intel/haswell/cfr.h>
+#include <drivers/intel/oc_mailbox/cfr.h>
 #include <drivers/option/cfr_frontend.h>
 #include <soc/cfr.h>
 
@@ -52,6 +53,7 @@ static struct sm_obj_form power = {
 static struct sm_obj_form *sm_root[] = {
 	&system,
 	&power,
+	&cpu_voltage,
 	NULL
 };
 
