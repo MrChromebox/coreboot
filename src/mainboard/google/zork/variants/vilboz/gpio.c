7847f3e657ac7fe2c781da7c6bdf8b503b0944fd
Matt DeVillier <matt.devillier@gmail.com>
2026-03-31 14:19:49 -0500

    mb/google/zork/vilboz: Set proximity INT as GPI for non-ChromeOS
    The proximity sensor is only used for SAR under ChromeOS. On
    non-ChromeOS builds it can trigger an IRQ storm under Windows, so
    configure GPIO_40 as a GPI to avoid leaving it in the baseboard
    programmed GPO/high state, which could potentially damage the
    sensor.
    
    Change-Id: Ie857fa6f9b916490b835c9715be3fe2b2b5450be
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>

--- src/mainboard/google/zork/variants/vilboz/gpio.c
+++ src/mainboard/google/zork/variants/vilboz/gpio.c
@@ -15,9 +15,17 @@ static const struct soc_amd_gpio bid_1_gpio_set_stage_ram[] = {
 };
 
 static const struct soc_amd_gpio vilboz_gpio_set_stage_ram[] = {
+/*
+ * The proximity sensor is only used for SAR under ChromeOS, and
+ * can cause an IRQ storm under Windows, so configure as a GPI
+ * for non-ChromeOS builds to avoid leaving in the baseboard-
+ * programmed state of GPO/high which could damage the sensor
+*/
 #if CONFIG(CHROMEOS)
 	/* P sensor INT */
 	PAD_INT(GPIO_40, PULL_NONE, LEVEL_LOW, STATUS_DELIVERY),
+#else
+	PAD_GPI(GPIO_40, PULL_NONE),
 #endif
 	/* LTE_RST_L */
 	PAD_GPO(GPIO_89, HIGH),
