8c83ce08e1cbeb0e27470b989bb5a92000930a8c
Matt DeVillier <matt.devillier@gmail.com>
2026-07-03 16:07:34 -0500

    ec/google/chromeec/cfr: Default automatic fan control to true
    If a board has a fan, set it to auto by default.
    
    Change-Id: If4b929165fa3ee8e79a78540ffb7acd5f30fd750
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>

--- src/ec/google/chromeec/cfr.h
+++ src/ec/google/chromeec/cfr.h
@@ -22,7 +22,7 @@ static const struct sm_object auto_fan_control = SM_DECLARE_BOOL({
 	.opt_name       = "auto_fan_control",
 	.ui_name        = "Automatic Fan Control",
 	.ui_helptext    = "Enable or disable automatic fan control.",
-	.default_value  = CONFIG(EC_GOOGLE_CHROMEEC_AUTO_FAN_CTRL),
+	.default_value  = true,
 }, WITH_CALLBACK(update_fan_control));
 
 static const struct sm_enum_value ec_backlight_values[] = {
